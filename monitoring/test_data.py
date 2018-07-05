f22724f483fb967fb7cbd143438bc3b70a4184e2
mudra <alexander.kobko@gmail.com>
2018-07-05 17:13:21 +0300

    first commit

---
+++ monitoring/test_data.py
@@ -0,0 +1,132 @@
+headers = {
+    'Authorization': "Basic OWIzYWFhZmJhOWZlNGY0Yzk1YzNiZTNlMWZlYWFlMzE6",
+    'Content-Type': "application/json",
+    'Cache-Control': "no-cache",
+    'Postman-Token': "0acb6f32-d856-44f4-9234-77c46981366b"
+    }
+
+#Ввести  tender_id вручну
+
+tender_id = 'ded768fa8bd2450097930c0aa7b9fd90'
+
+payload = {
+  "data": {
+    "reasons": [
+      "public",
+      "fiscal"
+    ],
+    "tender_id": tender_id,
+    "procuringStages": [
+      "awarding",
+      "contracting"
+    ],
+    "parties": [
+      {
+        "contactPoint": {
+          "name": "Oleksii Kovalenko",
+          "telephone": "0440000000"
+        },
+        "identifier": {
+          "scheme": "UA-EDR",
+          "id": "40165856",
+          "uri": "http://www.dkrs.gov.ua"
+        },
+        "name": "The State Audit Service of Ukraine",
+        "roles": [
+          "risk_indicator"
+        ],
+        "address": {
+          "countryName": "Ukraine",
+          "postalCode": "04070",
+          "region": "Kyiv",
+          "streetAddress": "Petra Sahaidachnoho St, 4",
+          "locality": "Kyiv"
+        }
+      }
+    ]
+  }
+}
+
+payload2 = {
+  "data": {
+    "decision": {
+      "date": "2018-01-02T01:05:00",
+      "description": "Опис підстав для здійсненнjjjjjjjjjjjjjjjjjjjjjjjjя моніторинг"
+    }
+  }
+}
+
+activate = {
+  "data": {
+    "status": "active"
+  }
+}
+
+msg = {
+  "data": {
+    "title": "Тайтл диалога5",
+    "description": "Описание диалога5"
+  }
+}
+
+
+conclusion = {
+  "data": {
+    "conclusion": {
+      "violationType": [
+        "documentsForm",
+        "corruptionAwarded"
+      ],
+      "description": "Ashes, ashes, we all fall down",
+      "stringsAttached": "Pocket full of posies",
+      "auditFinding": "Ring around the rosies",
+      "violationOccurred": True
+    }
+  }
+}
+
+
+adressed = {
+  "data": {
+    "status": "addressed"
+  }
+}
+
+
+stopped = {
+  "data": {
+    "status": "stopped",
+    "cancellation": {
+      "relatedParty": "be546d6519ee37a597df5c1ca71ddd18",
+      "description": "Complaint was created"
+    }
+  }
+}
+
+
+complete = {
+  "data": {
+    "status": "completed"
+  }
+}
+
+
+decline = {
+  "data": {
+    "status": "declined"
+  }
+}
+
+eliminationResolution = {
+  "data": {
+    "eliminationResolution": {
+      "description": "The award hasn't been fixed.",
+      "relatedParty": "f0e119b94ed33bd9b72caf2573503984",
+      "resultByType": {
+        "corruptionAwarded": "not_eliminated",
+        "documentsForm": "eliminated"
+      },
+      "result": "partly"
+    }
+  }
+}
---
+++ monitoring/test_monitoring.py
@@ -0,0 +1,70 @@
+import requests
+import json
+from monitoring.test_data import *
+
+
+# New draft only
+def draft_monitoring():
+
+    monitoring_id = requests.post('https://audit-api-sandbox.prozorro.gov.ua/api/2.4/monitorings', data=json.dumps(payload), headers=headers)
+    monitoring_id_long = monitoring_id.json()['data']['id']
+    monitoring_id_short = monitoring_id.json()['data']['monitoring_id']
+    return monitoring_id_long, monitoring_id_short
+
+
+# New monitoring + activation
+def active_monitoring():
+
+    monitoring_id = draft_monitoring()
+    requests.patch('https://audit-api-sandbox.prozorro.gov.ua/api/2.4/monitorings' + '/' + monitoring_id, data=json.dumps(payload2), headers=headers)
+    resp = requests.patch('https://audit-api-sandbox.prozorro.gov.ua/api/2.4/monitorings' + '/' + monitoring_id, data=json.dumps(activate), headers=headers)
+    return resp.json()['data']['id']
+
+
+# New monitoring + 5 posts
+def post_monitoring():
+
+    monitoring_id = active_monitoring()
+    for i in range(5):
+        requests.post('https://audit-api-sandbox.prozorro.gov.ua/api/2.4/monitorings/' + monitoring_id + '/' + 'posts', data=json.dumps(msg), headers=headers)
+    return monitoring_id
+
+
+# New monitoring ---> activate ----> addressed
+def make_conclusion_and_adress():
+
+    monitoring_id = post_monitoring()
+    requests.patch('https://audit-api-sandbox.prozorro.gov.ua/api/2.4/monitorings/' + monitoring_id, data=json.dumps(conclusion), headers=headers)
+    requests.patch('https://audit-api-sandbox.prozorro.gov.ua/api/2.4/monitorings/' + monitoring_id, data=json.dumps(adressed), headers=headers)
+    return monitoring_id
+
+
+# New monitoring ---> activate ----> addressed ---> stopped
+def stop_monitoring():
+    monitoring_id = make_conclusion_and_adress()
+    resp = requests.patch('https://audit-api-sandbox.prozorro.gov.ua/api/2.4/monitorings' + '/' + monitoring_id,
+                          data=json.dumps(stopped), headers=headers)
+    return monitoring_id
+
+
+# New monitoring ---> activate ----> addressed ---> complete
+def complete_monitoring():
+    monitoring_id = make_conclusion_and_adress()
+    resp = requests.patch('https://audit-api-sandbox.prozorro.gov.ua/api/2.4/monitorings' + '/' + monitoring_id,
+                          data=json.dumps(complete), headers=headers)
+    return resp.json()
+
+
+# New monitoring ---> activate ----> declined ---> closed
+def close_monitoring():
+    monitoring_id = active_monitoring()
+    requests.patch('https://audit-api-sandbox.prozorro.gov.ua/api/2.4/monitorings/' + monitoring_id,
+                   data=json.dumps(conclusion), headers=headers)
+    requests.patch('https://audit-api-sandbox.prozorro.gov.ua/api/2.4/monitorings/' + monitoring_id,
+                   data=json.dumps(eliminationResolution), headers=headers)
+    resp = requests.patch('https://audit-api-sandbox.prozorro.gov.ua/api/2.4/monitorings' + '/' + monitoring_id,
+                          data=json.dumps(decline), headers=headers)
+
+    return resp.json()
+
+print(draft_monitoring())
